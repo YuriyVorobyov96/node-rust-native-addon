f50193e9ec8f824bcc93fa12aff9fdad51a1f970
Yuriy Vorobyov <darthrevan1333@gmail.com>
2023-02-20 19:19:28 +0300

    Update lib.rs

--- native/src/lib.rs
+++ native/src/lib.rs
@@ -3,10 +3,6 @@ use neon::prelude::*;
 use neon::register_module;
 use num_cpus;
 
-// fn hello(mut cx: FunctionContext) -> JsResult<JsString> {
-//     Ok(cx.string("hello node"))
-// }
-
 fn thread_count(mut cx: FunctionContext) -> JsResult<JsNumber> {
     Ok(cx.number(num_cpus::get() as f64))
 }
